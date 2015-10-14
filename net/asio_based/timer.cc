da8e88ccb0d8698b7b811da72b2b16a917cd32d7
denisyq <denisyq@gmail.com>
2015-10-14 09:04:51 +0800

    update

---
+++ net/asio_based/timer.cc
@@ -0,0 +1,29 @@
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <iostream>
+#include <boost/thread/thread.hpp>
+using namespace std;
+using namespace boost::asio;
+
+void handle(const boost::system::error_code ec){
+	cout<<"timer run out"<<endl;
+}
+int main(int argc, char** argv){
+	io_service service;	
+	io_service::work iwork(service);
+	
+	deadline_timer timer(service, boost::posix_time::seconds(2));
+#if 0
+	// sync mode
+	// timer wait equals to sleep(2), it is blocking operation
+	timer.wait();
+	cout<<"wait n sec and then print"<<endl;
+#else
+	// async mode
+	timer.async_wait(handle);
+	cout<<"print me first"<<endl;
+	boost::this_thread::sleep(boost::posix_time::seconds(5));	
+	service.run();	//if forget this, async function handle will not be called.
+#endif
+	return 0;	
+}
