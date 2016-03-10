fa295de6bc1f7c76cbd7bbe8ad8335466540e370
denisyq <denisyq@gmail.com>
2016-03-10 14:59:38 +0800

    update

--- python/a.py
+++ python/a.py
@@ -1,18 +1,15 @@
-#!/usr/bin/python
-
-def foo(a,b):
-	print('{}==a,{}==b'.format(a,b))
+#!/usr/local/bin/python
 
 
-import re
-text=[]
-with open("tmp") as f:
-	for line in f.readlines():
-		add = [str for str in re.split(r'[\s,.:]\s*',line) if len(str) >=2]
-		text.extend(add)
+class A():
+	def __init__(self):
+		self.a = 1
+	
+class B(A):
+	def __init__(self):
+		super().__init__()
+	def get(self):
+		print(self.a)
 
-from collections import Counter
-c = Counter(text)
-for a,b in c.most_common(10):
-	print a,b
-
+b=B()
+print(b.get())
