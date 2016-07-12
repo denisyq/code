f54b3c7ecad8a780c743bdda494d24ae126af9f9
denisyq <denisyq@gmail.com>
2016-07-12 11:38:37 +0800

    Create boost_thread_asio.cc

---
+++ cpp/CPP/boost_thread_asio.cc
@@ -0,0 +1,9 @@
+boost::thread 
+boost::asio::io_service
+
+//They are big library and need .so/.a support \
+which means, not use #include <boost/*.hpp> could \
+work, which is more complicated.
+
+//I have built simple Thread class which could use
+//ASIO is more than a IO complexing archtecture.
